3edd4d8dafa4a3f1fa2fd26927b1c534dd7baa06
ZoDux <91859657+ZoDuxDev@users.noreply.github.com>
2022-05-22 16:20:59 -1000

    Update NewMainScript.lua

--- NewMainScript.lua
+++ NewMainScript.lua
@@ -97,7 +97,7 @@ if isfolder("vape/assets") == false then
 	makefolder("vape/assets")
 end
 
-local GuiLibrary = loadstring("https://raw.githubusercontent.com/ZoDuxDev/BoogaVapeV4/main/NewGuiLibrary.lua")()
+local GuiLibrary = loadstring("https://github.com/ZoDuxDev/BoogaVapeV4/blob/main/NewGuiLibrary.lua")()
 local translations = {}--loadstring(GetURL("translations/"..GuiLibrary["Language"]..".vapetranslation"))()
 local translatedlogo = false--pcall(function() return GetURL("translations/"..GuiLibrary["Language"].."/VapeLogo1.png") end)
 
